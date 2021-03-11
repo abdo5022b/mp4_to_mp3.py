71f2b13e20a78ec9a906769c9f160a00dc656a44
abdo5022b <67209340+abdo5022b@users.noreply.github.com>
2021-03-11 21:49:54 +0100

    Update mp4_to_mp3.py

--- mp4_to_mp3.py
+++ mp4_to_mp3.py
@@ -1,6 +1,6 @@
 import os
 base_dir = str(input("Path of file >>"))
-video = str(input("input video name >>"))
+video = str(input("Video name >>"))
 mp4_file = base_dir+"/"+video
 mp3_file = base_dir +"/audio.mp3"
 cmd = "ffmpeg -i {} -vn {}".format(mp4_file, mp3_file)
