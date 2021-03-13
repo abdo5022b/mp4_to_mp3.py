cc9e3c078e3153448cdb75f3756969415b8443aa
abdo5022b <67209340+abdo5022b@users.noreply.github.com>
2021-03-13 14:46:39 +0100

    Update mp4_to_mp3.py

--- mp4_to_mp3.py
+++ mp4_to_mp3.py
@@ -1,8 +1,13 @@
 import os
 base_dir = str(input("Path of file >>"))
-video = str(input("Video name >>"))
+video = str(input("input video name >>"))
 mp4_file = base_dir+"/"+video
 mp3_file = base_dir +"/audio.mp3"
 cmd = "ffmpeg -i {} -vn {}".format(mp4_file, mp3_file)
-os.system(cmd)
+try :
+	os.system(cmd)
+except :
+	print("ffmpeg not installed ")
+	os.system("pkg install ffmpeg")
+	
 #By Abdo5022b
